b7cd479caa3611008202f854e48d2e6e7a66557e
Justin Klass <justinklass55@gmail.com>
2021-09-20 23:25:30 -0400

    Successfully added axios to pull data on Covid 19 from https://api.coronavirus.data.gov.uk/v1/data

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// import Header from "./components/Header"
+import Axios from "axios";
 
 function App() {
+
+  const getStatistics = () => {
+    Axios.get("https://api.coronavirus.data.gov.uk/v1/data").then((response) => {
+      console.log(response)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello World
+      </h1>
+      <button onClick={getStatistics}>TEST</button>
+
     </div>
   );
 }
